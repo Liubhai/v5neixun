594eb91a286cbe2cf7121d9a27d6d7ea896070d9
刘邦海 <754230638@qq.com>
2020-04-23 10:17:20 +0800

    code:直播间素材

---
+++ EdulineV5/LivePage/LiveRoomViewController.h
@@ -0,0 +1,17 @@
+//
+//  LiveRoomViewController.h
+//  EdulineV5
+//
+//  Created by 刘邦海 on 2020/4/23.
+//  Copyright © 2020 刘邦海. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LiveRoomViewController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
