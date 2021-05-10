18fece618599f76bd59c21782e623e74c2cda424
刘邦海 <754230638@qq.com>
2021-05-10 14:18:40 +0800

    code:我的考试-考试结果页面，全部解析和错题解析，最后一个题的底部“上一题”按钮居中显示

--- EdulineV5/FindPage/ExamPage/ExamResultDetailViewController.m
+++ EdulineV5/FindPage/ExamPage/ExamResultDetailViewController.m
@@ -737,6 +737,7 @@
                                 [_nextExamBtn setTitleColor:EdlineV5_Color.themeColor forState:0];
                                 [_nextExamBtn setImage:nil forState:0];
                                 _previousExamBtn.hidden = NO;
+                                [_previousExamBtn setCenterX:MainScreenWidth / 2.0];
                                 [_previousExamBtn setTitleColor:EdlineV5_Color.textThirdColor forState:0];
                                 [_previousExamBtn setImage:Image(@"exam_last") forState:0];
                             } else {
@@ -1094,6 +1095,7 @@
                 [_nextExamBtn setTitleColor:EdlineV5_Color.themeColor forState:0];
                 [_nextExamBtn setImage:nil forState:0];
                 _previousExamBtn.hidden = NO;
+                [_previousExamBtn setCenterX:MainScreenWidth / 2.0];
                 [_previousExamBtn setTitleColor:EdlineV5_Color.textThirdColor forState:0];
                 [_previousExamBtn setImage:Image(@"exam_last") forState:0];
             } else {
@@ -1226,6 +1228,7 @@
                             [_nextExamBtn setTitleColor:EdlineV5_Color.themeColor forState:0];
                             [_nextExamBtn setImage:nil forState:0];
                             _previousExamBtn.hidden = NO;
+                            [_previousExamBtn setCenterX:MainScreenWidth / 2.0];
                             [_previousExamBtn setTitleColor:EdlineV5_Color.textThirdColor forState:0];
                             [_previousExamBtn setImage:Image(@"exam_last") forState:0];
                         } else {
