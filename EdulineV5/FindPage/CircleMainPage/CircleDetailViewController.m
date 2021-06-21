4e2133596f76f61205ad6b88c43590c6c32d8bdc
刘邦海 <754230638@qq.com>
2021-06-21 10:36:55 +0800

    code:取消动态详情页table滚动悬浮效果

--- EdulineV5/FindPage/CircleMainPage/CircleDetailViewController.m
+++ EdulineV5/FindPage/CircleMainPage/CircleDetailViewController.m
@@ -268,19 +268,6 @@
     }];
 }
 
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView
-{
-    CGFloat sectionHeaderHeight = 42;
-    if (scrollView.contentOffset.y <= sectionHeaderHeight && scrollView.contentOffset.y >= 0)
-    {
-        scrollView.contentInset = UIEdgeInsetsMake(- scrollView.contentOffset.y, 0, 0, 0);
-    }
-    else if (scrollView.contentOffset.y >= sectionHeaderHeight)
-    {
-        scrollView.contentInset = UIEdgeInsetsMake(- sectionHeaderHeight, 0, 0, 0);
-    }
-}
-
 // MARK: - 点赞代理(评论点赞)
 - (void)zanComment:(CircleDetailCommentCell *)cell {
     // 此时需要判断登录
